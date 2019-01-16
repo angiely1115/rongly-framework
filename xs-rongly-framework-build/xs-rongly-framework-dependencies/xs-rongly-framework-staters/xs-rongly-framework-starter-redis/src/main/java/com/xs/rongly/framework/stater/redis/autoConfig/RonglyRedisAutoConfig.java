8c6e3334a61c1f4711c1205b483b1eb1995e312d
lvrongzhuan <lvrongzhuan@cnhnkj.com>
2019-01-16 19:28:54 +0800

    redisTemplate配置

--- xs-rongly-framework-build/xs-rongly-framework-dependencies/xs-rongly-framework-staters/xs-rongly-framework-starter-redis/src/main/java/com/xs/rongly/framework/stater/redis/autoConfig/RonglyRedisAutoConfig.java
+++ xs-rongly-framework-build/xs-rongly-framework-dependencies/xs-rongly-framework-staters/xs-rongly-framework-starter-redis/src/main/java/com/xs/rongly/framework/stater/redis/autoConfig/RonglyRedisAutoConfig.java
@@ -122,6 +122,24 @@ public class RonglyRedisAutoConfig {
     }
 
     @Primary
+    @Bean("redisTemplateByte")
+    public RedisTemplate<byte[], byte[]> redisTemplateByte(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+     /*   // 使用Jackson2JsonRedisSerialize 替换默认序列化
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        // 设置value的序列化规则和 key的序列化规则
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setDefaultSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);*/
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+    @Primary
     @Bean
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         StringRedisTemplate redisTemplate = new StringRedisTemplate(redisConnectionFactory);
---
+++ xs-rongly-framework-build/xs-rongly-framework-dependencies/xs-rongly-framework-staters/xs-rongly-framework-stater-web/src/main/java/com/xs/rongly/framework/stater/web/exception/BizException.java
@@ -0,0 +1,11 @@
+package com.xs.rongly.framework.stater.web.exception;
+
+/**
+ * @Author: lvrongzhuan
+ * @Description:
+ * @Date: 2019/1/16 14:58
+ * @Version: 1.0
+ * modified by:
+ */
+public class BizException {
+}
