be208e9e6e09dc0f3de60684c61e6b91b35c4e7e
lvrongzhuan <lvrongzhuan@cnhnkj.com>
2019-01-28 22:13:20 +0800

    自动扫描配置

---
+++ xs-rongly-framework-build/xs-rongly-framework-dependencies/xs-rongly-framework-staters/xs-rongly-framework-starter-dubbo/src/main/java/com/xs/rongly/framework/stater/dubbo/config/DubboAutoConfig.java
@@ -0,0 +1,11 @@
+package com.xs.rongly.framework.stater.dubbo.config;
+
+/**
+ * @Author: lvrongzhuan
+ * @Description:
+ * @Date: 2019/1/28 21:28
+ * @Version: 1.0
+ * modified by:
+ */
+public class DubboAutoConfig {
+}
---
+++ xs-rongly-framework-build/xs-rongly-framework-dependencies/xs-rongly-framework-staters/xs-rongly-framework-stater-security/src/main/java/com/xs/rongly/framework/stater/security/autoConfig/annotation/SecurityScan.java
@@ -0,0 +1,11 @@
+package com.xs.rongly.framework.stater.security.autoConfig.annotation;
+
+/**
+ * @Author: lvrongzhuan
+ * @Description:
+ * @Date: 2019/1/28 21:34
+ * @Version: 1.0
+ * modified by:
+ */
+public @interface SecurityScan {
+}
